4e0087883fd794842694dc6c6a126680db68529d
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2025-05-06 23:07:14 +0700

    add css baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import TextField  from "@mui/material/TextField";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 function App() {
   return (
     <>
+      <span>Every</span>
        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+       <AccountBoxIcon color="success"/>
+       <AccountBoxIcon color="action"/>
     </>
   );
 }
