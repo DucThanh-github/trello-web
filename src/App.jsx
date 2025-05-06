135594e178223c63f6881ee5298ec1548617120a
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2025-05-07 00:01:23 +0700

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import TextField  from "@mui/material/TextField";
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-
+import Button from '@mui/material/Button'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <span>Every</span>
-       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-       <AccountBoxIcon color="success"/>
-       <AccountBoxIcon color="action"/>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography color="text.secondary">Test Typography</Typography>
+      <AccountBoxIcon color="success" />
+      <AccountBoxIcon color="action" />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
